8cdd670bbdf295f8dd331e1717b3bbbd78d13931
tohaddid <54992797+VolgarDiablo@users.noreply.github.com>
2024-06-17 21:28:27 +0300

    done ticket

--- lesson_3/lesson_3.1/js/main.js
+++ lesson_3/lesson_3.1/js/main.js
@@ -1,3 +1,20 @@
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("Page loaded");
-});
+//Додати туди невеликий скрипт, мета якого – вивести всі відомі вам типи даних у консоль. Використовуючи оператор typeof та console.log
+const ageUser = 22;
+const nameUser = "Anton";
+const isTrue = true;
+const person = {
+  name: nameUser,
+  age: ageUser,
+};
+const bigNumber = 1234567890123456789012345678901234567890n;
+const uniqueID = Symbol("id");
+const emptyValue = null;
+
+console.log(typeof ageUser); //"number"
+console.log(typeof nameUser); //"string"
+console.log(typeof isTrue); //"boolean"
+console.log(typeof person); //"object"
+console.log(typeof bigNumber); // "bigint"
+console.log(typeof uniqueID); //"symbol"
+console.log(typeof emptyValue); //"object"
+console.log(typeof variable); //"undefined"
