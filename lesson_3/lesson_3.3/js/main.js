b478096cadee63138235d5aaecea1d239fb47da5
tohaddid <54992797+VolgarDiablo@users.noreply.github.com>
2024-06-17 22:08:04 +0300

    done ticket 3.3

--- lesson_3/lesson_3.3/js/main.js
+++ lesson_3/lesson_3.3/js/main.js
@@ -1,3 +1,32 @@
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("Page loaded");
-});
+//Розкласти за цифрами п'ятизначне число і вивести у вихідному порядку через пробіл. Приклад:
+//10369
+//1 0 3 6 9
+
+//Option 1
+const number = 10369;
+
+let string = number.toString();
+let newString = string[0];
+
+console.log(string.length);
+for (let i = 1; i < string.length; i++) {
+  newString = newString + " " + string[i];
+}
+console.log(newString);
+
+// Option 2
+const number = 10369;
+const stringNumber = number.toString();
+const digitsArray = stringNumber.split("").join(" ");
+console.log(digitsArray);
+
+//Option 3
+const number = 10369;
+let temp = number;
+let result = "";
+while (temp > 0) {
+  const digit = temp % 10;
+  result = digit + " " + result;
+  temp = Math.floor(temp / 10);
+}
+console.log(result);
