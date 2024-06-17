9a5464e8be86a67a5f5ae65c338b14e69ee9d96a
tohaddid <54992797+VolgarDiablo@users.noreply.github.com>
2024-06-17 21:40:59 +0300

    done ticket 3.2

--- lesson_3/lesson_3.2/js/main.js
+++ lesson_3/lesson_3.2/js/main.js
@@ -1,3 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("Page loaded");
-});
+//Отримати від користувача 3 рядки та вивести їх у довільному порядку однією командою (шаблонні рядки);
+let name = prompt("Please enter your name", "<Entering your name here>");
+let soname = prompt("Please enter your soname", "<Entering your soname here>");
+let age = prompt("Please enter your age", "<Entering your age here>");
+
+console.log(
+  `Your name is a ${name}, your soname is a ${soname} and your age is a ${age}`
+);
