9aa7bbf793ef4b4e5beb919ad3b9729606f0f7ce
tohaddid <54992797+VolgarDiablo@users.noreply.github.com>
2024-06-20 22:45:45 +0300

    done ticket 4.1

---
+++ lesson_4/lesson_4.1/js/main.js
@@ -0,0 +1,4 @@
+// За допомогою prompt запитати “ім'я користувача”.
+// За допомогою alert вивести "Hello, John! How are you?" , де “John” це те, що ввів користувач
+const nameUser = prompt("What`s ur name?");
+alert(`Hello, ${nameUser}! How are you?`);
