c7e7d1d5845589b25b6984990da2ab80acc115ee
tohaddid <54992797+VolgarDiablo@users.noreply.github.com>
2024-06-28 09:17:01 +0300

    done ticket 6.3
    Implemented the removeElement(array, item) function to remove the item from the array array using the ready-made splice() function

---
+++ lesson_6/lesson_6.3/js/main.js
@@ -0,0 +1,18 @@
+// Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
+// Наприклад:
+// const array = [1, 3, 4, 6, 2, 5, 7];
+// removeElement(array,4);
+// console.log(array); // Результат: [1, 3, 6, 2, 5, 7]
+
+function removeElement(array, item) {
+    const index = array.indexOf(item);
+    if (index > -1) {
+        array.splice(index, 1);
+    }
+}
+
+const array = [1, 3, 4, 6, 2, 5, 7];
+
+removeElement(array, 4);
+
+console.log(array);
