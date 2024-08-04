5eae34183f76fe9bb3457d2279843899cb6cfa82
tohaddid <54992797+VolgarDiablo@users.noreply.github.com>
2024-08-04 11:02:09 +0300

    create form

---
+++ lesson_13/lesson_13/js/main.js
@@ -0,0 +1,7 @@
+const formData = document.getElementById("form");
+const name = document.getElementById("name");
+const message = document.getElementById("smthText");
+const phone = document.getElementById("phoneInput");
+const emailInput = document.getElementById("emailInput");
+const btnSendMsg = document.getElementById("btnSendMsg");
+
