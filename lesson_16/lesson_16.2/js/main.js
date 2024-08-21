6a609173258f416239ad11b05b39253b7b1f25a3
tohaddid <54992797+VolgarDiablo@users.noreply.github.com>
2024-08-21 08:47:55 +0300

    update directory
    delete 16.1-16.3

--- lesson_16/lesson_16.2/js/main.js
+++
@@ -1,7 +0,0 @@
-const parentContainer = document.getElementById("parent-container");
-
-parentContainer.addEventListener("click", () => {
-  if (e.target.tagName === "BUTTON") {
-    console.log(`You clicked: ${e.target.textContent}`);
-  }
-});
