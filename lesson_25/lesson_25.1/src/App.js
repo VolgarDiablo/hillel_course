65f635fcd86dbb6764893eab6a1fde65158f4d9b
tohaddid <54992797+VolgarDiablo@users.noreply.github.com>
2024-10-19 11:48:08 +0300

    .

---
+++ lesson_25/lesson_25.1/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Vote from "./components/vote/vote";
+
+function App() {
+  return (
+    <div className="App">
+      <Vote />
+    </div>
+  );
+}
+
+export default App;
