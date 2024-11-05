a4f8891237096b867cbccd4eb95824414c2592b2
tohaddid <54992797+VolgarDiablo@users.noreply.github.com>
2024-11-05 16:59:46 +0200

    .

--- lesson_27/lesson_27.1/src/App.js
+++ lesson_27/lesson_27.1/src/App.js
@@ -1,9 +1,29 @@
-import ToDo from "./component/ToDo";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./components/header/Header";
+import Content from "./components/content/Content";
+import "./App.css";
+import "./Theme.css";
 
 function App() {
+  const [theme, setTheme] = useState(() => {
+    return localStorage.getItem("theme") || "light";
+  });
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
   return (
-    <div className="App">
-      <ToDo />
+    <div className="App" data-theme={theme}>
+      <Router>
+        <Header toggleTheme={toggleTheme} theme={theme} />
+        <Content />
+      </Router>
     </div>
   );
 }
